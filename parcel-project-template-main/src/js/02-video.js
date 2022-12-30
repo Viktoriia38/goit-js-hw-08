9c41d6d501a87817760e7116447da244c452924f
Viktoriia38 <bva922@ukr.net>
2022-12-31 01:42:27 +0200

    Task 2-3

--- parcel-project-template-main/src/js/02-video.js
+++ parcel-project-template-main/src/js/02-video.js
@@ -1,1 +1,13 @@
+const player = new Vimeo.Player('vimeo-player');
+const storageKey = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(storageKey);
 
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+function onPlay({ seconds }) {
+  localStorage.setItem(storageKey, seconds);
+}
--- parcel-project-template-main/src/js/03-feedback.js
+++ parcel-project-template-main/src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', _.throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
 
+const formData = {};
+
+function onFormData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm(event) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+(function dataLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
